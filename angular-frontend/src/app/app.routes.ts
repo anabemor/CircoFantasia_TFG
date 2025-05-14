23a472d0a7b38e089748dc1c7f6f89b865a443bb
anabemor <anabelenmoreno2@hotmail.com>
2025-05-14 11:32:24 +0200

    Limpieza del proyecto. Quito taks y añado auth

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-//import { TasksComponent } from './modules/tasks/tasks.component';
 import { LandingComponent } from './modules/landing/landing.component';
-import { LoginComponent } from './modules/auth/login/login.component';
+import { LoginComponent } from './modules/login/login/login.component';
 import { AdminDashboardComponent } from './modules/admin/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
     {path: "", component: LandingComponent},
-    //{path: "tasks", component: TasksComponent},
     {path: "login", component: LoginComponent},
     {path: 'admin', component: AdminDashboardComponent}
 ];
---
+++ angular-frontend/src/app/modules/gestor/gestor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestor',
+  imports: [],
+  templateUrl: './gestor.component.html',
+  styleUrl: './gestor.component.css'
+})
+export class GestorComponent {
+
+}
