ffaec61c7fa5c60054a05d282f994cc3fa48e52b
anabemor <anabelenmoreno2@hotmail.com>
2025-05-19 18:48:58 +0200

    Instalación Material Angular Toast

--- angular-frontend/src/app/modules/admin/usuarios/usuarios.component.ts
+++ angular-frontend/src/app/modules/admin/usuarios/usuarios.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Usuario, UsuariosService } from '../../../shared/services/usuarios.service';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatSnackBarModule],
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
@@ -15,7 +16,7 @@ export class UsuariosComponent {
   usuarioSeleccionado: Usuario | null = null;
   mostrarModal: boolean = false;
 
-  constructor(private usuariosService: UsuariosService) {}
+  constructor(private usuariosService: UsuariosService,  private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.cargarUsuarios();
@@ -57,20 +58,36 @@ export class UsuariosComponent {
 
     const { id } = this.usuarioSeleccionado;
 
-    const callback = () => {
+    const callback = (mensaje: string) => {
       this.cargarUsuarios();
       this.cerrarModal();
+      this.snackBar.open(mensaje, 'Cerrar', {
+        duration: 3000,
+        verticalPosition: 'top',
+      });
     };
 
     if (id) {
       this.usuariosService.actualizarUsuario(id, this.usuarioSeleccionado).subscribe({
-        next: callback,
-        error: (err) => console.error('Error al editar usuario:', err),
+        next: () => callback('Usuario actualizado correctamente'),
+        error: (err) => {
+          console.error('Error al editar usuario:', err);
+          this.snackBar.open('Error al actualizar usuario', 'Cerrar', {
+            duration: 3000,
+            verticalPosition: 'top',
+          });
+        },
       });
     } else {
       this.usuariosService.crearUsuario(this.usuarioSeleccionado).subscribe({
-        next: callback,
-        error: (err) => console.error('Error al crear usuario:', err),
+        next: () => callback('Usuario creado correctamente'),
+        error: (err) => {
+          console.error('Error al crear usuario:', err);
+          this.snackBar.open('Error al crear usuario', 'Cerrar', {
+            duration: 3000,
+            verticalPosition: 'top',
+          });
+        },
       });
     }
   }
