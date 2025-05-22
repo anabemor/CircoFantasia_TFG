bba9d4353730a24846de37d028fe875e7fe82dca
anabemor <anabelenmoreno2@hotmail.com>
2025-05-22 22:00:06 +0200

    Creo activity.php para crear la tabla actividades en base de datos.

---
+++ symfony-backend/app/src/Entity/Activity.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActivityRepository::class)]
+class Activity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $descripcion = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $fechaInicio = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $fechaFin = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $activa = true;
+
+    // --- Getters y Setters ---
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): static
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    public function getFechaInicio(): ?\DateTimeInterface
+    {
+        return $this->fechaInicio;
+    }
+
+    public function setFechaInicio(\DateTimeInterface $fechaInicio): static
+    {
+        $this->fechaInicio = $fechaInicio;
+        return $this;
+    }
+
+    public function getFechaFin(): ?\DateTimeInterface
+    {
+        return $this->fechaFin;
+    }
+
+    public function setFechaFin(\DateTimeInterface $fechaFin): static
+    {
+        $this->fechaFin = $fechaFin;
+        return $this;
+    }
+
+    public function isActiva(): bool
+    {
+        return $this->activa;
+    }
+
+    public function setActiva(bool $activa): static
+    {
+        $this->activa = $activa;
+        return $this;
+    }
+}
